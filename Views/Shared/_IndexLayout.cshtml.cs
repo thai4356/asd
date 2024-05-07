143940e206fa7c2ee8a527d0d238a6b785b1b332
zhong xina <117057017+thai4356@users.noreply.github.com>
2024-05-07 19:45:54 +0700

    lan dau

---
+++ Views/Shared/_IndexLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Italian_Restaurant.Pages.Shared
+{
+    public class _IndexLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
